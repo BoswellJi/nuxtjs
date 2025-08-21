220890578b642c6cf9678546a8dd93c621a61ee1
mingzhuang.ji <mingzhuang.ji@ly.com>
2025-08-21 18:57:08 +0800

    chore: 添加数据库

--- server/api/test.ts
+++ server/api/test.ts
@@ -1,4 +1,6 @@
-import prisma from '../lib/prisma';
+import { PrismaClient } from '../generated/prisma-client';
+
+const prisma = new PrismaClient();
 
 export default defineEventHandler(async event => {
   const users = await prisma.user.create({
